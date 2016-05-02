8ac961b85a4af653387dfd9c7571a16943d24d54
Eduardo Emery <edudu_ecs@hotmail.com>
2016-05-02 01:56:00 -0300

    Added solution to L1-01

--- L1-01-softmax.py
+++ L1-01-softmax.py
@@ -1,23 +1,51 @@
 # Softmax
 
-scores = [3.0, 1.0, 0.2]
-
 import numpy as np 
+import matplotlib.pyplot as plt 
+
+scores = np.array([[1, 2, 3, 6],
+                   [2, 4, 5, 6],
+                   [3, 8, 7, 6]])
+
+
+
 
 def softmax(x):
 	"""Compute softmax values for x"""
-	den = 0
-	for i in x:
-		den += np.exp(i)
-	for i in range(len(x)):
-		x[i] = np.exp(x[i]) / den
-	return x
+	return np.exp(x) / np.sum(np.exp(x), axis = 0)
+
+
+
+
+def softmax_alt(x):
+	"""
+	Compute softmax values for x
+	This is an alternative solution that works properly
+	"""
+	x = np.array(x)
+
+	if len(x.shape) >= 2:
+		x = (x + 0.0).T
+		for i in range(len(x)):
+
+			den = np.sum(np.exp(x[i]))
+			for j in range(len(x[0])):
+				x[i,j] = np.divide(np.exp(x[i,j]) , den)
+
+		return x.T
+
+	else:
+		den = np.sum(np.exp(x) + 0.0)
+		for i in range(len(x)):
+			x[i] = np.exp(x[i]) / den
+		return x
+
+
 
 
 print(softmax(scores))
 
 # Plot softmax curves
-import matplotlib.pyplot as plt 
 x = np.arange(-2.0, 6.0, 0.1)
 scores = np.vstack([x, np.ones_like(x), 0.2*np.ones_like(x)])
 
