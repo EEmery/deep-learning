13fb436c2b2d1a9e836715025ba3a51dc33ad1e1
Eduardo Emery <edudu_ecs@hotmail.com>
2016-05-02 02:03:18 -0300

    Added solution to L1-01

--- L1-01-softmax.py
+++ L1-01-softmax.py
@@ -3,10 +3,7 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 
-scores = np.array([[1, 2, 3, 6],
-                   [2, 4, 5, 6],
-                   [3, 8, 7, 6]])
-
+scores = np.array([3.0, 1.0, 0.2])
 
 
 
@@ -44,6 +41,8 @@ def softmax_alt(x):
 
 
 print(softmax(scores))
+print(softmax(scores*10))
+print(softmax(scores/10))
 
 # Plot softmax curves
 x = np.arange(-2.0, 6.0, 0.1)
