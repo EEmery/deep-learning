5605c43b5c49d8608601b9161826cf7750b5811e
EEmery <edudu_ecs@hotmail.com>
2016-10-24 21:47:09 -0300

    added a time based reward system

--- cart-pole.py
+++ cart-pole.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 
 # Cares about OpenAI Gym stuff
 MAX_EPISODES = 10000
-MAX_TIMESTEPS = 1000
+MAX_TIMESTEPS = 100
 
 env = gym.make("CartPole-v0")		# Sets the simulation to the Cart Pole one
 
@@ -20,17 +20,28 @@ with graph.as_default():
 		past_rewards = tf.placeholder(shape=[None, 1], dtype=tf.float32)
 
 		# Hidden layer 1
-		weights1 = tf.Variable(tf.truncated_normal([4, 5]))
+		weights1 = tf.Variable(tf.truncated_normal([4, 10]))
 		hidden1 = tf.nn.relu(tf.matmul(observations, weights1))
 
 		# Output layer
-		weights2 = tf.Variable(tf.truncated_normal([5, 1]))
+		weights2 = tf.Variable(tf.truncated_normal([10, 1]))
 		actions = tf.nn.sigmoid(tf.matmul(hidden1, weights2))
 
 		# Training stuff
 		loss = -tf.reduce_mean(tf.log(past_actions - actions) * past_rewards)
-		optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.5).minimize(loss)
+		optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(loss)
 
+
+# Changes rewards
+def change_rewards(rewards):
+	gamma = 0.99
+	temp = 0
+	for i in reversed(xrange(0, rewards.size)):
+		temp = temp*gamma + rewards[i]
+		rewards[i] = temp
+	rewards -= np.mean(rewards)
+	rewards /= np.std(rewards)
+	return rewards
 
 # Runs the session
 with tf.Session(graph=graph) as sess:
@@ -41,16 +52,17 @@ with tf.Session(graph=graph) as sess:
 
 	for episode in range(MAX_EPISODES):
 		
-		observation = env.reset()												# Reset the env on the end of every episode
+		observation = env.reset()							# Reset the env on the end of every episode
 		episode_observations, episode_actions, episode_rewards = [], [], []		# Cleans our buffers
 		
 		for step in range(MAX_TIMESTEPS):
 
 			# Here we run the neural network
-			env.render()														# Renders the frame on every timestep
-			observation = np.reshape(observation, [1, 4])						# Reshapes the observation tensor for proper usage
+			if step > 12:
+				env.render()							# Renders the frame on every timestep
+			observation = np.reshape(observation, [1, 4])				# Reshapes the observation tensor for proper usage
 			guess = sess.run([actions], feed_dict={observations:observation})	# Asks the neural net to guess an action
-			action = 1 if (guess > 0.5) else 0 									# Generates an action from that guess
+			action = 1 if (guess > 0.5) else 0 					# Generates an action from that guess
 			
 			# Saves observations and actions pair for latter neural net training
 			episode_observations.append(observation)
@@ -66,9 +78,10 @@ with tf.Session(graph=graph) as sess:
 			# Train the neural net at the end of every episode
 			if done:
 
+				# Format observations, actions and rewards for training
 				episode_observations = np.vstack(episode_observations)
 				episode_actions = np.vstack(episode_actions)
-				episode_rewards = np.vstack(episode_rewards)
+				episode_rewards = np.vstack(change_rewards(episode_rewards))
 
 				_, training_loss = sess.run([optimizer, loss], feed_dict={observations:episode_observations, past_actions:episode_actions, past_rewards:episode_rewards})
 				if episode%10 == 0:
