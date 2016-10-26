1c450fd866f6a4128fece712db8eb8a1d9645764
EEmery <edudu_ecs@hotmail.com>
2016-10-26 05:07:20 -0300

    tried to train by batches of episodes

--- cart-pole.py
+++ cart-pole.py
@@ -6,6 +6,7 @@ import tensorflow as tf
 # Cares about OpenAI Gym stuff
 MAX_EPISODES = 10000
 MAX_TIMESTEPS = 100
+BATCH_SIZE = 50
 
 env = gym.make("CartPole-v0")		# Sets the simulation to the Cart Pole one
 
@@ -29,19 +30,27 @@ with graph.as_default():
 
 		# Training stuff
 		loss = -tf.reduce_mean(tf.log(past_actions - actions) * past_rewards)
-		optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(loss)
+		optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
+
+		# For updating the gradients at the end of every batch
+		tvars = tf.trainable_variables()
+		newGrads = tf.gradients(loss, tvars)
+		W1Grad = tf.placeholder(tf.float32)
+		W2Grad = tf.placeholder(tf.float32)
+		updateGrads = optimizer.apply_gradients(zip([W1Grad, W2Grad], tvars))
 
 
 # Changes rewards
 def change_rewards(rewards):
 	gamma = 0.99
 	temp = 0
-	for i in reversed(xrange(0, rewards.size)):
+	for i in reversed(xrange(0, len(rewards))):
 		temp = temp*gamma + rewards[i]
 		rewards[i] = temp
 	rewards -= np.mean(rewards)
 	rewards /= np.std(rewards)
 	return rewards
+
 
 # Runs the session
 with tf.Session(graph=graph) as sess:
@@ -50,19 +59,23 @@ with tf.Session(graph=graph) as sess:
 	tf.initialize_all_variables().run()
 	print("Initialized")
 
+	gradBuffer = sess.run(tvars)
+	for ix, grad in enumerate(gradBuffer):
+		gradBuffer[ix] = grad * 0
+
 	for episode in range(MAX_EPISODES):
 		
-		observation = env.reset()							# Reset the env on the end of every episode
+		observation = env.reset()												# Reset the env on the end of every episode
 		episode_observations, episode_actions, episode_rewards = [], [], []		# Cleans our buffers
 		
 		for step in range(MAX_TIMESTEPS):
 
 			# Here we run the neural network
 			if step > 12:
-				env.render()							# Renders the frame on every timestep
-			observation = np.reshape(observation, [1, 4])				# Reshapes the observation tensor for proper usage
+				env.render()													# Renders the frame on every timestep
+			observation = np.reshape(observation, [1, 4])						# Reshapes the observation tensor for proper usage
 			guess = sess.run([actions], feed_dict={observations:observation})	# Asks the neural net to guess an action
-			action = 1 if (guess > 0.5) else 0 					# Generates an action from that guess
+			action = 1 if (guess > 0.5) else 0 									# Generates an action from that guess
 			
 			# Saves observations and actions pair for latter neural net training
 			episode_observations.append(observation)
@@ -83,10 +96,15 @@ with tf.Session(graph=graph) as sess:
 				episode_actions = np.vstack(episode_actions)
 				episode_rewards = np.vstack(change_rewards(episode_rewards))
 
-				_, training_loss = sess.run([optimizer, loss], feed_dict={observations:episode_observations, past_actions:episode_actions, past_rewards:episode_rewards})
-				if episode%10 == 0:
-					print "Episode " + str(episode) + " endend at step " + str(step) + " with loss " + str(training_loss)
-				break
+				# Gets and saves the gradients of this episode
+				tGrad = sess.run(newGrads, feed_dict={observations:episode_observations, past_actions:episode_actions, past_rewards:episode_rewards})
+				for ix, grad in enumerate(tGrad):
+					gradBuffer[ix] += grad
 
-# TODO:
-#	- Penalize actions rewards at the end of the episode
+				# Updates the gradients at the end of every batch
+				if episode%BATCH_SIZE == 0:
+					sess.run(updateGrads, feed_dict={W1Grad:gradBuffer[0], W2Grad:gradBuffer[1]})
+					for ix, grad in enumerate(gradBuffer):
+						gradBuffer[ix] = grad * 0
+					print "Episode " + str(episode) + " endend at step " + str(step)
+				break
